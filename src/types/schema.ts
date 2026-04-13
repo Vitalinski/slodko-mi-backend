6a984a415d414293461e60144789383395959d6f
Vitalii <vitaliy.kutoviy2000@gmail.com>
2026-04-13 07:09:34 +0200

    feat: include order items in order retrieval and improve schema formatting

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,22 +1,28 @@
 import { OrderStatus } from "@prisma/client";
 
 export const productsQuerySchema = {
-  type: 'object',
+  type: "object",
   properties: {
-    categories: { type: 'array', items: { type: 'string' } },
-    searchValue: { type: 'string' },
-    onlyPopular: { type: 'boolean' },
-    page: { type: 'number', default: 1 },
-    limit: { type: 'number', default: 8 },
+    categories: { type: "array", items: { type: "string" } },
+    searchValue: { type: "string" },
+    onlyPopular: { type: "boolean" },
+    page: { type: "number", default: 1 },
+    limit: { type: "number", default: 8 },
   },
 };
 
 export const ordersQuerySchema = {
-  type: 'object',
+  type: "object",
   properties: {
-    orderStatuses: { type: 'array', items: { type:OrderStatus } },
-    searchValue: { type: 'string' },
-    page: { type: 'number', default: 1 },
-    limit: { type: 'number', default: 8 },
+    orderStatuses: {
+      type: "array",
+      items: {
+        type: "string",
+        enum: Object.values(OrderStatus),
+      },
+    },
+    searchValue: { type: "string" },
+    page: { type: "number", default: 1 },
+    limit: { type: "number", default: 8 },
   },
 };
